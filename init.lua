b11575fa7ce0bba463344c7cdd80fa8c4612dd05
Pedro Silva <pedro.silva@fira.com.br>
2023-11-13 07:55:45 -0300

    feat: define remote url

--- init.lua
+++ init.lua
@@ -11,7 +11,7 @@ return {
     show_changelog = true, -- show the changelog after performing an update
     auto_quit = false, -- automatically quit the current session after a successful update
     remotes = { -- easily add new remotes to track
-      --   ["remote_name"] = "https://remote_url.come/repo.git", -- full remote url
+      ["origin"] = "https://github.com/pedronr03/astronvim-config", -- full remote url
       --   ["remote2"] = "github_user/repo", -- GitHub user/repo shortcut,
       --   ["remote3"] = "github_user", -- GitHub user assume AstroNvim fork
     },
